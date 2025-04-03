8faa8689ce12e554c70c3999081ff2cc8e9ecf3d
inna-bw <inna.kravchenko@bestway.com.ua>
2025-04-03 17:39:17 +0300

    main update

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,40 @@
+class Canvas {
+	constructor () {
+		this.canvasElement = document.getElementById('canvasGrid');
+		
+		this.context = this.canvasElement.getContext('2d')
+		this.setSize()
+		window.addEventListener('resize', () => {
+			this.setSize()
+		})
+	}
+
+	getContext () {
+		return this.context;
+	}
+		
+	setSize () {
+		this.canvasElement.width = this.canvasElement.parentNode.clientWidth;
+		this.canvasElement.height = this.canvasElement.parentNode.clientWidth;
+	}
+
+	getSize () {
+		return {
+			width: this.canvasElement.width,
+			height: this.canvasElement.height,
+		}
+	}
+
+	clear () {
+		const {width, height} = this.getSize();
+		this.context.clearRect(0, 0, width, height)
+		this.context.fillStyle = '#282222'
+		this.context.fillRect(0, 0, width, height)
+	}
+}
+
+const canvas = new Canvas();
+canvas.clear();
 // smoth scroll
 document.addEventListener('DOMContentLoaded', function(){
 	Array.prototype.forEach.call(document.querySelectorAll("[data-scrollto]"), function(button){
